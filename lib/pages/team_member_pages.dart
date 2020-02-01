3613a934a281b594708172d201fca5c8601f9cc1
Piyu <siddhesh.parab28@gmail.com>
2020-02-01 23:20:28 +0530

    new

--- lib/pages/team_member_pages.dart
+++ lib/pages/team_member_pages.dart
@@ -1,5 +1,6 @@
 
-import 'dart:async';
+
+
 import 'package:flutter/material.dart';
 import 'package:listview/model/member.dart';
 import 'package:listview/resources/colors.dart';
@@ -31,85 +32,85 @@ class TeamMembersPage extends StatelessWidget {
           }
           else
             return
-              ListView.separated(
-                itemBuilder: (context, index) {
-                  DocumentSnapshot mypost = snapshot.data.documents[index];
-                  print('${mypost['title']}');
-                 return Row(
-                    children: <Widget>[
-                      Material(
-                        elevation: size_8,
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(size_12)),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(size_12)),
-                          child: Image.network(
-                            '${mypost['image']}',
-                            height: 100,
-                            width: 80,
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: size_20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              
+                ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context,index){
+                      DocumentSnapshot mypost=snapshot.data.documents[index];
+                      return Stack(
                         children: <Widget>[
-                          RichText(
-                            text: TextSpan(text: '${mypost['title']}'
-                                .toUpperCase(),
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: size_20,
-                                  fontWeight: FontWeight.w900,
-                                )),
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                child: Padding(
+                                  padding: EdgeInsets.all(12.0),
+                                  child: Material(
+                                    elevation: 8,
+                                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                                    child: Column (children: <Widget>[
+                                      Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        height: 195.0,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.only(topLeft: Radius.circular(12),topRight: Radius.circular(12)),
+                                        child: Image.network('${mypost['image']}',
+                                          fit: BoxFit.fill,
+                                        ),
+                                      )
+                                      ),
+                                      Container(
+                                        alignment: Alignment.topLeft,
+                                        padding: EdgeInsets.only(top: 0.0,right: 5.0,left: 5.0,bottom: 5.0),
+                                        child: Column
+                                          ( crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                                          SizedBox(height: 10),
+                                          Chip(
+                                            label: Text('${mypost['title']}'),backgroundColor: Colors.blue.shade900,
+                                            labelStyle: TextStyle(fontSize: 10.0,fontWeight: FontWeight.bold,color: Colors.white),
+                                          ),
+                                         Text('${mypost['subtitle']}'),
+                                          SizedBox(height: 10.0),
+                                          Text ('${mypost['title']}',
+                                            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                                          )
+                                        ],),
+                                      ),
+                               /*       Container(
+                                        alignment: Alignment.topRight,
+                                        padding: EdgeInsets.only(
+                                          top: MediaQuery.of(context).size.height*.27,
+                                          left: MediaQuery.of(context).size.height*.22,
+                                        ),
+                                        child: Container(
+                                          width: MediaQuery.of(context).size.width,
+                                          child: CircleAvatar (
+                                            backgroundColor: Colors.blue,
+                                            child: Icon(Icons.share,color: Colors.white,size: 20.0,),
+                                          ),
+                                        ),
+                                      )
+*/
+                                    ],),
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
-                          SizedBox(height: size_8),
-                          Text('${mypost['subtitle']}',
-                            style: TextStyle
-                              (
-                              color: blueColor,
-                              fontSize: size_12,
-                              fontWeight: FontWeight.w800,
-                            ),)
+                          Positioned(
+                            top: 50,
+                            right: 25,
+                            bottom: -25,
+                            child: CircleAvatar (
+                              backgroundColor: Colors.blue,
+                              child: Icon(Icons.share,color: Colors.white,size: 20.0,),
+                            ),
+                          )
                         ],
-                      )
-                    ],
-                  );
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              child: Material(
-                color: Colors.white,
-                elevation: 14.0,
-                borderRadius: BorderRadius.circular(24.0),
-                shadowColor: Color(0x802196f3),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      height: 250,
-                      width: 350,
-                      child: ClipRRect(
-                        borderRadius: new BorderRadius.circular(24.0),
-                        child: Image(
-                          fit: BoxFit.contain,
-                          alignment: Alignment.topCenter,
-                          image: NetworkImage('${mypost['image']}'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-                },
-                separatorBuilder: (context, index) {
-                 return SizedBox(
-                    height: size_40,
-                  );
-                }, itemCount: snapshot.data.documents.length,);
+                      );
+                    });
+
+
                  }),
 
             );
