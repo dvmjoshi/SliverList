fef048a8dbf73a000ba4e29dac8c836a167ecc6f
Piyu <siddhesh.parab28@gmail.com>
2020-02-03 21:39:51 +0530

    new

--- lib/pages/team_member_pages.dart
+++ lib/pages/team_member_pages.dart
@@ -1,105 +1,154 @@
-
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:listview/model/member.dart';
+import 'package:listview/myflexiableappbar.dart';
 import 'package:listview/resources/colors.dart';
 import 'package:listview/resources/sizes.dart';
 import 'package:listview/resources/text_styles.dart';
 import 'package:listview/widgets/clipped_image.dart';
 import 'package:listview/widgets/member_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:listview/myappbar.dart';
-import 'package:listview/myflexiableappbar.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class TeamMembersPage extends StatelessWidget {
-  var number;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-              title: MyAppBar(),
-              backgroundColor: Colors.white,
-              pinned: true,
-              expandedHeight: 280.0,
-              flexibleSpace: FlexibleSpaceBar(
-                background: MyFlexiableAppBar(),
-              )
+      appBar: AppBar(
+        leading: Icon(Icons.keyboard_backspace, color: Colors.black),
+        title: Text(
+          "Our Team",
+          style: appBarTextStyle,
+        ),
+        centerTitle: false,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+    /*  body: StreamBuilder (
+          stream: Firestore.instance.collection('posts').snapshots(),
+          builder: (context,snapshot) {
+            if (!snapshot.hasData) {
+              return const Text('Loading..');
+            }
+            else
+              return
+                ListView.separated(
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot mypost = snapshot.data.documents[index];
+                    print('${mypost['title']}');
+                    return Row(
+                      children: <Widget>[
+                        Material(
+                          elevation: size_8,
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(size_12)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(size_12)),
+                            child: Image.network(
+                              '${mypost['image']}',
+                              height: 100,
+                              width: 80,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: size_20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            RichText(
+                              text: TextSpan(text: '${mypost['title']}'
+                                  .toUpperCase(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: size_20,
+                                    fontWeight: FontWeight.w900,
+                                  )),
+                            ),
+                            SizedBox(height: size_8),
+                            Text('${mypost['subtitle']}',
+                              style: TextStyle
+                                (
+                                color: blueColor,
+                                fontSize: size_12,
+                                fontWeight: FontWeight.w800,
+                              ),)
+                          ],
+                        )
+                      ],
+                    );
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        child: Material(
+                          color: Colors.white,
+                          elevation: 14.0,
+                          borderRadius: BorderRadius.circular(24.0),
+                          shadowColor: Color(0x802196f3),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                height: 250,
+                                width: 350,
+                                child: ClipRRect(
+                                  borderRadius: new BorderRadius.circular(24.0),
+                                  child: Image(
+                                    fit: BoxFit.contain,
+                                    alignment: Alignment.topCenter,
+                                    image: NetworkImage('${mypost['image']}'),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: size_120,
+                    );
+                  }, itemCount: snapshot.data.documents.length,);
+
+
+          }),
+*/
+
+    body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          title: AppBar(),
+          backgroundColor: Colors.white,
+          pinned: true,
+          expandedHeight: 280.0,
+          flexibleSpace: FlexibleSpaceBar(
+            background: MyFlexiableAppBar(),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context,int index){
-              return StreamBuilder (
-                stream:Firestore.instance.collection('posts').snapshots(),
-                builder: (context,snapshot) {
+        ),
+        StreamBuilder(
+          stream: Firestore.instance.collection('posts').snapshots(),
+          builder: (context,snapshot){
+            if(!snapshot.hasData){
+              return SliverList(
+                delegate: SliverChildBuilderDelegate((context,int index){
                   DocumentSnapshot mypost=snapshot.data.documents[index];
-                  number = snapshot.data.documents.length;
-                  if(!snapshot.hasData){
-                    return Text('loading');
-                  }
-                  else{
-                    return Container(
-                      child: FadeInImage.memoryNetwork(
+                  return Container(
+                    child: FadeInImage.memoryNetwork(
                         placeholder: kTransparentImage,
                         image: '${mypost['image']}',
                         width: MediaQuery.of(context).size.width,
                         fit: BoxFit.fitWidth,
-                      ),
-                    );
-                  }
-                },
+                    ),
+                  );
+                },childCount: snapshot.data.documents.length),
               );
-            },
-                childCount:number
-            ),
-
-          )
-          ////////////////////////////////////////
-          ////////////////////////////////////////
-          ////////////////////////////////////////
-          ///////////////////////////////////////
-          /* StreamBuilder (
-      stream: Firestore.instance.collection('posts').snapshots(),
-      builder: (context,snapshot){*/
-          /* if(!snapshot.hasData){
-
-        }
-        else return SliverList(
-          delegate: SliverChildBuilderDelegate((con,int index){
-            DocumentSnapshot mypost=snapshot.data.documents[index];
-            return Container(
-              child: FadeInImage.memoryNetwork(
-                  placeholder:kTransparentImage,
-                  image: '${mypost['image']}',
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.fitWidth,),
-            );
-          },childCount: snapshot.data.documents.length),
-        );*/
-
-          /*  if(snapshot.hasData){
-           return SliverList(
-            delegate: SliverChildBuilderDelegate((con,int index){
-              DocumentSnapshot mypost=snapshot.data.documents[index];
-              return Container(
-                child: FadeInImage.memoryNetwork(
-                  placeholder:kTransparentImage,
-                  image: '${mypost['image']}',
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.fitWidth,),
-              );
-            },childCount: snapshot.data.documents.length),
-          );
-        }
-      },
-    )*/
-          /////////////////////////////////////////
-          /////////////////////////////////////
-          ///////////////////////////////////
-        ],
-      ),
-            );
-        }
+            }
+          },
+        )
+      ],
+    ),
+    );
   }
+}
